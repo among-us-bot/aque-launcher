cef33addf0f4cf6ad5064e8bec75de10ca00e97c
Epic <tagepicuwu@gmail.com>
2020-11-22 18:46:17 +0000

    Fix: Launcher script using launcher.py as the worker count

--- launch.py
+++ launch.py
@@ -1,11 +1,11 @@
 from sys import argv
 from os import system
-
+print(f"Launching using {argv[1]} workers")
 commands = [
     "git pull",
     "git submodule update",
     "docker-compose build",
     "docker-compose down",
-    f"docker-compose up -d --scale worker={argv[0]}"
+    f"docker-compose up -d --scale worker={argv[1]}"
 ]
 [system(command) for command in commands]
